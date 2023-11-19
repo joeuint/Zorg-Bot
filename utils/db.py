bf0111be228dd66620386b4187f7a1c5468765e8
Joseph <94891082+joseph-umana@users.noreply.github.com>
2023-11-19 17:00:29 -0500

    Refactor init_db function to support username and password

--- utils/db.py
+++ utils/db.py
@@ -1,12 +1,16 @@
 """Utilities relating to the database"""
 import motor.motor_asyncio as motor
 
-def init_db(hostname: str, port: int, db_name: str) -> motor.AsyncIOMotorDatabase:
+def init_db(hostname: str, port: int, db_name: str, username: str = '', password: str = '') -> motor.AsyncIOMotorDatabase:
     """Initializes and returns the MongoDB connection
 
     Returns:
         AsyncIOMotorDatabase: The Motor database instance
     """
     # pylint: disable=C0103
-    db = motor.AsyncIOMotorClient(hostname, port)[db_name]
+
+    if username or password:
+        db = motor.AsyncIOMotorClient(f'mongodb://{username}:{password}@{hostname}:{port}/{db_name}')
+    else:
+        db = motor.AsyncIOMotorClient(f'mongodb://{hostname}:{port}/{db_name}')
     return db
