af8b57d5f5fa4b80350235e8a479b5039b626742
Joseph <94891082+joseph-umana@users.noreply.github.com>
2023-11-19 18:08:40 -0500

    Add print statement to see connection uri

--- utils/db.py
+++ utils/db.py
@@ -11,6 +11,7 @@ def init_db(hostname: str, port: int, db_name: str, username: str = '', password
 
     if username != '' or password != '':
         db = motor.AsyncIOMotorClient(f'mongodb://{username}:{password}@{hostname}:{port}')[db_name]
+        print(f'mongodb://{username}:{password}@{hostname}:{port}')
     else:
         db = motor.AsyncIOMotorClient(f'mongodb://{hostname}:{port}')[db_name]
     return db
